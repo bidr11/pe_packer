4b2f1a10aaec52d21caacce30b9dd0b092b2cae1
bidr <badr.jebbari3615@gmail.com>
2023-09-16 13:51:39 +0100

    fully working with AES encryption and ZLIB compression

--- packer_executable/main.py
+++ packer_executable/main.py
@@ -1,8 +1,10 @@
 import argparse
+from base64 import b64encode
 import lief
 import os
 import zlib
 import struct
+from Crypto.Cipher import AES
 
 def align(x, al):
     """ return <x> aligned to <al> """
@@ -17,11 +19,32 @@ def pad_data(data, al):
     return data + ([0] * (align(len(data), al) - len(data)))
 
 
+def pad(plain_text):
+    block_size = AES.block_size;
+    number_of_bytes_to_pad = block_size - len(plain_text) % block_size
+    ascii_string = chr(number_of_bytes_to_pad)
+    padding_str = number_of_bytes_to_pad * ascii_string
+    padded_plain_text = plain_text + padding_str.encode()
+    return padded_plain_text
+    
+
+def encrypt(plain_text):
+    plain_text = pad(plain_text)
+    iv = b"abcdef9876543210"
+    key = b"0123456789abcdef"
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    encrypted_text = cipher.encrypt(plain_text)
+    return encrypted_text
+
 def pack_data(data):
     size = len(data)
-    size_in_bytes = struct.pack("<I", size)
+    decompressed_size = struct.pack("<I", size)
+    # print(encrypt(data).hex())
     print(size, hex(size))
-    return size_in_bytes + zlib.compress(data, 2)  # 2 is the compression level, 0 is no compression
+    compressed_data = decompressed_size + zlib.compress(data, 2) # 2 is the compression level, 0 is no compression
+    size = len(compressed_data)
+    compressed_size = struct.pack("<I", size)
+    return compressed_size + encrypt(compressed_data)
     # return data
 
 
@@ -50,6 +73,7 @@ if __name__ == "__main__":
 
     # create the section in lief
     # packed_data = pack_data(list(input_PE_data))  # pack the input file data
+    print(pack_data(input_PE_data).hex())
     packed_data = list(pack_data(input_PE_data))  # pack the input file data
 
     packed_data = pad_data(packed_data,
